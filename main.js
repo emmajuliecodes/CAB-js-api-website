9e53a9419e621d209d1b2e422dc1398c0ffc30e7
emmajuliecodes <emmajuliedavidson@gmail.com>
2023-06-22 20:53:54 +0200

    completed build of continent filter; began build of filter by checkbox

--- main.js
+++ main.js
@@ -23,49 +23,48 @@ const fetchData = () => {
 // ASSIGNING LENGTH FOR LOOP
 // let countriesCount = countriesData.length;
 
-
 //  CREATING LOOP & BUILDING TABLE (within a function)
 
 const createHtmlTable = (countriesData) => {
-    let countryCount = countriesData.length;
+	let countryCount = countriesData.length;
 
-		let myTable = document.getElementById("countriesTable");
+	let myTable = document.getElementById("countriesTable");
 
-		myTable.innerText = "";
-		countriesData.forEach((country, i) => {
-			const tr = document.createElement("tr");
+	myTable.innerText = "";
+	countriesData.forEach((country, i) => {
+		const tr = document.createElement("tr");
 
-			//     // If creating header here rather than appending to table in html doc
-			//     // const createTHead(myData);
-			//     // const row = header.insertRow (0);
-			//     // clearInterval.innerHTML ="<b>Countries</ b>"
+		//     // If creating header here rather than appending to table in html doc
+		//     // const createTHead(myData);
+		//     // const row = header.insertRow (0);
+		//     // clearInterval.innerHTML ="<b>Countries</ b>"
 
-			const tdCountry = document.createElement("td");
-			tdCountry.innerText = countriesData[i].name.common;
+		const tdCountry = document.createElement("td");
+		tdCountry.innerText = countriesData[i].name.common;
 
-			const tdCapital = document.createElement("td");
-			tdCapital.innerText = countriesData[i].capital;
+		const tdCapital = document.createElement("td");
+		tdCapital.innerText = countriesData[i].capital;
 
-			const tdContinent = document.createElement("td");
-			tdContinent.innerText = countriesData[i].continents;
+		const tdContinent = document.createElement("td");
+		tdContinent.innerText = countriesData[i].continents;
 
-			const tdFlag = document.createElement("td");
-			tdFlag.innerText = countriesData[i].flag;
+		const tdFlag = document.createElement("td");
+		tdFlag.innerText = countriesData[i].flag;
 
-			myTable.appendChild(tr);
-			tr.appendChild(tdCountry);
-			tr.appendChild(tdCapital);
-			tr.appendChild(tdContinent);
-			tr.appendChild(tdFlag);
-		});
-	}
+		myTable.appendChild(tr);
+		tr.appendChild(tdCountry);
+		tr.appendChild(tdCapital);
+		tr.appendChild(tdContinent);
+		tr.appendChild(tdFlag);
+	});
+};
 
 // CREATING DROPDOWN
 
 // createContinentDropdown = (countriesData) => {
 // 	console.log(countriesData);
 //     const dropdown = document.getElementById("continentDropdown");
-    
+
 //     const continentsArray = countriesData.map((country)=> {
 //         return country.continents[0];
 //     });
@@ -78,84 +77,99 @@ const createHtmlTable = (countriesData) => {
 // 		const dropdownOption = document.createElement("option");
 // 		dropdownOption.innerText = continent;
 // 		dropdown.appendChild(dropdownOption)
-// 	}) 
-		
+// 	})
+
 // 	}
 
 createContinentDropdown = (countriesData) => {
-    const dropdown = document.getElementById("continentDropdown");
+	const dropdown = document.getElementById("continentDropdown");
 
 	// ADD TO.STRING TO ACCOUNT FOR CONTINENT NAME NOT BEING OBJECT IN CONTINENTS
-    const continentsArray = countriesData.map((country)=> {
-        return country.continents.toString();
-    });
-//   
-//  HACKY WAY TO RETURN UNIQUES - WON'T WORK WITH ALL DATA 
-// 	const continentsArray = countriesData.map((country)=> {
-//         return country.continents[0];
-//     });
+	const continentsArray = countriesData.map((country) => {
+		return country.continents.toString();
+	});
+	//
+	//  HACKY WAY TO RETURN UNIQUES - WON'T WORK WITH ALL DATA
+	// 	const continentsArray = countriesData.map((country)=> {
+	//         return country.continents[0];
+	//     });
 
-//  ALTERNATIVE TO RETURN UNIQUES - FLATMAP FUNCTION
-// 	const continentsArray = countriesData.flatMap((country)=> {
-//         return country.continents;
-//     });
+	//  ALTERNATIVE TO RETURN UNIQUES - FLATMAP FUNCTION
+	// 	const continentsArray = countriesData.flatMap((country)=> {
+	//         return country.continents;
+	//     });
 
 	console.log(continentsArray);
 
 	const uniqueContinentsArray = [...new Set(continentsArray)];
 	console.log(uniqueContinentsArray);
 
-
 	uniqueContinentsArray.forEach((continent) => {
 		const dropdownOption = document.createElement("option");
 		dropdownOption.innerText = continent;
-		dropdown.appendChild(dropdownOption)
-	}) 
-		
-	}
+		dropdown.appendChild(dropdownOption);
+	});
+};
 
 // ADDING EVENT LISTENERS
 
 const setEventListeners = (countriesData) => {
-	document.querySelector("#continentDropdown")
-	.addEventListener ("change", () => {
-		filterByContinentDropdown(countriesData);
-	
-	});
-
+	document
+		.querySelector("#continentDropdown")
+		.addEventListener("change", () => {
+			filterByContinentDropdown(countriesData);
+		});
+	document
+		.querySelector("#independenceQuery")
+		.addEventListener("change", (event) => {
+			filterByIndependenceCheckbox(countriesData);
+		});
 };
 
 // FILTER BY DROPDOWN
 
 const filterByContinentDropdown = (countriesData) => {
-// const { continents } = countriesData;
-
 	const selectedContinent = document.querySelector("#continentDropdown").value;
-	console.log(selectedContinent);
-//  Prints chosen continent in clg
-	
-	const filteredByContinent = countriesData.filter((continent) => {
-		return (countriesData.continent=== selectedContinent || selectedContinent === "all");
-	
+
+	const filteredByContinent = countriesData.filter((country, i) => {
+		return (
+			countriesData[i].continents.toString() === selectedContinent ||
+			selectedContinent === "all"
+		);
 	});
-	console.log(filteredByContinent);
-// TO FIX: clg displays all continents but shows none when a filter is selected
 
 	createHtmlTable(filteredByContinent);
-
-
 };
 
+// FILTER BY INDEPENDENCE CHECKBOX
 
+const filterByIndependenceCheckbox = (countriesData) => {
+	console.log(countriesData[1].independent);
+	// const checkbox = document.querySelector("#independenceQuery").value;
+
+	// // const independentCountries = countriesData.filter(country => countriesData.independent === "true");
+	// const independentCountries = countriesData.map((country, i) => {
+	// 	if (countriesData[i].independent === "true") {
+	// 		return independentCountries;
+	// 	}
+	// });
+
+	const independentCountries = countriesData[i].filter(
+		countriesData[i].independent.value === "true"
+	);
+
+	console.log(independentCountries);
+};
 
 // FUNCTION CONTROLLER
 
 const controller = (countriesData) => {
 	// console.log("in controller", countriesData);
 	createHtmlTable(countriesData);
-    createContinentDropdown(countriesData);
-	setEventListeners(countriesData); 
+	createContinentDropdown(countriesData);
+	setEventListeners(countriesData);
 	filterByContinentDropdown(countriesData);
+	filterByIndependenceCheckbox(countriesData);
 };
 
 fetchData();
